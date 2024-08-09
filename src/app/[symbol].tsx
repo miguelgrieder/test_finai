21025a9ad4c0ca4b86a07e55f4442b18fe465731
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-08-08 23:34:41 -0300

    feat: Query the stock details

--- src/app/[symbol].tsx
+++ src/app/[symbol].tsx
@@ -1,17 +1,35 @@
 import {View, Text} from '@/src/components/Themed';
 import React from "react";
 import {useLocalSearchParams} from "expo-router";
-import top5 from '@/assets/data/top5.json'
 import StockListItem from "@/src/components/StockListItem";
 import {Stack} from "expo-router";
 import Graph from "@/src/components/Graph";
+import { useQuery, gql } from '@apollo/client';
+import { ActivityIndicator } from 'react-native';
 
+const query = gql`
+  query MyQuery($symbol: String) {
+    quote(symbol: $symbol) {
+      name
+      symbol
+      close
+      percent_change
+    }
+  }
+`;
 const StockDetails = () => {
-    const {symbol} = useLocalSearchParams()
+  const {symbol} = useLocalSearchParams()
 
-    const stock = top5[symbol];
+  const { data, loading, error } = useQuery(query, { variables: { symbol } });
 
-    if (!stock) return <Text>Stock with symbol {symbol} could not be found</Text>;
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Stock with symbol {symbol} could not be found</Text>;
+  }
+  const stock = data.quote;
 
     return (
         <View style={{padding: 10}}>
