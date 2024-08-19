0335f765a68b6c465a63479c5745dd8324029921
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-08-18 21:40:36 -0300

    feat: Uses env for sensitive informations, creates a generator script for api/config.yaml

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -9,7 +9,7 @@ const client = new ApolloClient({
   uri: 'https://willangie.us-east-a.ibm.stepzen.net/api/twelvedata/__graphql',
   cache: new InMemoryCache(),
   headers: {
-      Authorization: `Apikey apikey`,
+      Authorization: `${process.env.EXPO_PUBLIC_APOLO_STEPZEN_APIKEY}`,
   }
 });
 
